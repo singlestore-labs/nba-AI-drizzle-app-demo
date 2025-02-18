3d05c79b929f2bee9bfe29d68ca492d798195fae
prodrigues <prodrigues@singlestore.com>
2025-02-18 00:52:49 +0000

    remove embeddings column

--- db/schema.ts
+++ db/schema.ts
@@ -13,7 +13,6 @@ export const commentaryTable = singlestoreTable("commentary_table", {
   id: bigint("id", { mode: "bigint" }).primaryKey().autoincrement(),
   timestamp: timestamp().notNull(),
   commentary: text().notNull(),
-  embedding: vector({ dimensions: 1536 }),
   latency: float().notNull(),
   warriors_win_probability: float().notNull(),
   warriors_score: float().notNull(),
